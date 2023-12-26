26600549d1aacc5c5201c6277386583ada9ab7c3
Murray Jack <murrayjack@gmail.com>
2023-12-26 22:04:23 +1100

    Initial Commit

---
+++ bs/bsApp.swift
@@ -0,0 +1,17 @@
+//
+//  bsApp.swift
+//  bs
+//
+//  Created by Murray Jack on 26/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct bsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
